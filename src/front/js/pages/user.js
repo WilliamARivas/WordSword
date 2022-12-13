5f29c1a5c51a737ece1855030b28250232ce1bc5
JaneshkaF <janeshkaf@aol.com>
2022-12-13 17:30:25 +0000

    .

--- src/front/js/pages/user.js
+++ src/front/js/pages/user.js
@@ -19,7 +19,7 @@ export const User = (props) => {
           <div className="col-4"></div>
           <div className="col-6">
             <div className="col">
-              <div className="card">
+              {/* <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">Card title</h5>
                   <p className="card-text">
@@ -28,7 +28,7 @@ export const User = (props) => {
                     little bit longer.
                   </p>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
