271329ed36067d7073608b600f645fcd2d7e6f47
faithward <faithward24@gmail.com>
2022-12-13 02:55:19 +0000

    changed background on output

--- src/front/js/pages/outputcopy.js
+++ src/front/js/pages/outputcopy.js
@@ -13,17 +13,19 @@ export const OutputCopy = () => {
   const handleTextClick = (event) => {
     event.target.style.backgroundColor = "rgb(255,255,0)";
   };
+  //two clicks should change it back
 
   return (
-    <div className="container py-4">
+    <div className="container py-4 bg-light">
       <div className="d-flex justify-content-center">
-        <h1>OUTPUT PAGE</h1>
+        <h1>Your Condensed Text:</h1>
       </div>
       <div>
         {store.displayText.map((item, index) => {
           return (
-            <span className="flashyText" id={index} onClick={handleTextClick}>
+            <span className="flashyText text-dark" id={index} onClick={handleTextClick}>
               {item}
+              <br></br>
             </span>
           );
         })}
@@ -33,6 +35,20 @@ export const OutputCopy = () => {
       <br></br>
       <br></br>
       <br></br>
+      <div className="text-center">
+       {
+        //conditionally render if user  is verfied
+        !store.verifiedUser ? (
+          <p>
+            Still don't have a WordSword account to save your work?{" "}
+            <Link to="/create">Click here </Link>
+            to get set up!
+          </p>
+        ) : (
+          <></> //add link to lead them to personal page
+        )
+      }
+    </div>
       <div className="d-flex justify-content-center">
         <span>:Save Word Sword Text:</span>
         <span>:Save Highlighted Text:</span>
